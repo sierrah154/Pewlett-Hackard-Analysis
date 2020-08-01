c97b85bff7145e04e5d30e58132b23ac7467bd02
Sierra Harris <sierra.harris154@outlook.com>
2020-08-01 13:32:30 -0700

    NEW: Imported csv data into tables, created retirement_info.csv. MODIFIED: schema.sql and queries.sql-now include queries that created the retirement_info.csv.

--- schema.sql
+++ schema.sql
@@ -21,7 +21,6 @@ CREATE TABLE dept_manager (
 	emp_no INT NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
@@ -40,7 +39,6 @@ CREATE TABLE dept_emp (
 	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
@@ -49,9 +47,15 @@ CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
 );
 
 SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+
+-- titles PRIMARY KEY/FOREIGN KEY errors; check solutions.
+
+SELECT * FROM titles;
