6206965779dbfd36ffd69494207018bd07c5b8eb
Sierra Harris <sierra.harris154@outlook.com>
2020-08-08 18:12:15 -0700

    NEW: Adding data files, dept_info, emp_info, manager_info. MODIFIED: queries.sql to include queries to form new data tables.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -108,10 +108,12 @@ INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
 -- Check the table
 SELECT * FROM retirement_info;
 
 -- Joining departments and dept_manager tables
+-- Using aliases for code readability
 SELECT d.dept_name,
      dm.emp_no,
      dm.from_date,
@@ -128,7 +130,7 @@ ri.last_name,
 	de.to_date
 FROM retirement_info as ri
 LEFT JOIN dept_emp as de
-on ri.emp_no = de.emp_no;
+ON ri.emp_no = de.emp_no;
 
 SELECT ri.emp_no,
 	ri.first_name,
@@ -148,3 +150,51 @@ LEFT JOIN dept_emp as de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
+
+-- List 1, salaries per employee
+SELECT e.emp_no,
+	e.first_name,
+e.last_name,
+	e.gender,
+	s.salary,
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+	AND (de.to_date = '9999-01-01');
+SELECT * FROM emp_info
+-- 1. What’s going on with the salaries?
+
+-- List 2, managers per department
+SELECT  dm.dept_no,
+		d.dept_name,
+        dm.emp_no,
+	ce.last_name,
+	ce.first_name,
+		dm.from_date,
+		dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+INNER JOIN departments AS d
+ON (dm.dept_no = d.dept_no)
+INNER JOIN current_emp AS ce
+ON (dm.emp_no = ce.emp_no);
+-- 2. Why are there only five active managers for nine departments?
+		
+-- List 3, department retirees
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+d.dept_name
+INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+-- 3. Why are some employees appearing twice?
